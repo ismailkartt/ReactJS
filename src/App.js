bd8c5b46c12558ded2e51a05c112e8fd95821e51
İsmail Kart <ismailkartt@gmail.com>
2024-07-20 05:57:26 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+const App = () => {
+    return(
+        <div>
+        {/* <HelloWorld/>
+        <Jsx1/>
+        <Jsx2/>
+        <Jsx3/>
+        <Jsx4/>
+        <Jsx5/>
+        <Jsx6/>
+        <Style1/>
+        <Style2/>
+        <Style3/>
+        <Style4/>
+        */}
+        
+        <Clock1/>
+
+        </div>
+    )
+} 
+export default App;
+
